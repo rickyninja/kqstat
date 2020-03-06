297f992344bc28e97f77d8866a2d0aaf42361d11
Jeremy Singletary <jeremys@rickyninja.net>
2020-03-06 13:54:42 -0700

    move conn attribs to own type to work better with ServerHTTP

--- mock/kqstatd/server.go
+++ mock/kqstatd/server.go
@@ -16,12 +16,7 @@ import (
 
 // Replay replays stats from an io.Reader on repeat.
 type Replay struct {
-	*websocket.Conn
-	reader          *bufio.Reader
-	buffer          []byte
-	failedKeepAlive chan struct{}
-	wmutex          *sync.Mutex
-	rmutex          *sync.Mutex
+	buffer []byte
 }
 
 // NewReplay constructs a *Replay object using an io.Reader as its input source.
@@ -31,37 +26,34 @@ func NewReplay(r io.Reader) (*Replay, error) {
 		return nil, err
 	}
 	re := &Replay{
-		buffer:          buf,
-		failedKeepAlive: make(chan struct{}),
-		wmutex:          new(sync.Mutex),
-		rmutex:          new(sync.Mutex),
+		buffer: buf,
 	}
-	re.resetReader()
 	return re, nil
 }
 
 // ServeHTTP does http.Handler.
 func (r *Replay) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	var upgrader = websocket.Upgrader{} // use default options
-	c, err := upgrader.Upgrade(rw, req, nil)
+	ws, err := upgrader.Upgrade(rw, req, nil)
 	if err != nil {
 		log.Print("upgrade:", err)
 		return
 	}
-	defer c.Close()
-	r.Conn = c
+	con := newConn(ws)
+	defer con.Close()
 	done := make(chan struct{})
 	defer func() { done <- struct{}{} }()
-	go r.doKeepAlives(done)
+	go con.doKeepAlives(done)
+	reader := r.getReader()
 	for {
 		select {
-		case <-r.failedKeepAlive:
+		case <-con.failedKeepAlive:
 			return
 		default:
-			line, err := r.reader.ReadBytes('\n')
+			line, err := reader.ReadBytes('\n')
 			if err != nil {
 				if err == io.EOF {
-					r.resetReader()
+					reader = r.getReader()
 					if len(line) < 1 {
 						continue
 					}
@@ -70,8 +62,7 @@ func (r *Replay) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 					return
 				}
 			}
-			fmt.Printf("writing line: <%s>\n", string(line))
-			err = r.WriteMessage(websocket.TextMessage, line)
+			err = con.WriteMessage(websocket.TextMessage, line)
 			if err != nil {
 				log.Println("write: ", err)
 				return
@@ -80,31 +71,50 @@ func (r *Replay) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func (r *Replay) resetReader() {
+// getReader returns a copy of the original input as an io.Reader for each connection.
+func (r *Replay) getReader() *bufio.Reader {
 	cp := make([]byte, len(r.buffer))
 	n := copy(cp, r.buffer)
 	if n < len(r.buffer) {
 		log.Printf("Failed to copy buffer: got %d want %d\n", n, len(r.buffer))
-		return
 	}
-	r.reader = bufio.NewReader(bytes.NewBuffer(cp))
+	return bufio.NewReader(bytes.NewBuffer(cp))
+}
+
+// conn manages the websocket connection.
+type conn struct {
+	*websocket.Conn
+	failedKeepAlive chan struct{}
+	wmutex          *sync.Mutex
+	rmutex          *sync.Mutex
+}
+
+// newConn creates a *conn from a websocket.
+func newConn(ws *websocket.Conn) *conn {
+	return &conn{
+		Conn:            ws,
+		failedKeepAlive: make(chan struct{}),
+		wmutex:          new(sync.Mutex),
+		rmutex:          new(sync.Mutex),
+	}
 }
 
 // ReadMessage wraps websocket.Conn.ReadMessage with a mutex.
-func (r *Replay) ReadMessage() (int, []byte, error) {
-	r.rmutex.Lock()
-	defer r.rmutex.Unlock()
-	return r.Conn.ReadMessage()
+func (c *conn) ReadMessage() (int, []byte, error) {
+	c.rmutex.Lock()
+	defer c.rmutex.Unlock()
+	return c.Conn.ReadMessage()
 }
 
 // WriteMessage wraps websocket.Conn.WriteMessage with a mutex.
-func (r *Replay) WriteMessage(messageType int, data []byte) error {
-	r.wmutex.Lock()
-	defer r.wmutex.Unlock()
-	return r.Conn.WriteMessage(messageType, data)
+func (c *conn) WriteMessage(messageType int, data []byte) error {
+	c.wmutex.Lock()
+	defer c.wmutex.Unlock()
+	return c.Conn.WriteMessage(messageType, data)
 }
 
-func (r *Replay) doKeepAlives(done <-chan struct{}) {
+// doKeepAlives sends keep alive messages, and expects the proper response from the peer.
+func (c *conn) doKeepAlives(done <-chan struct{}) {
 	ticker := time.NewTicker(600 * time.Millisecond)
 	defer ticker.Stop()
 	for {
@@ -112,29 +122,30 @@ func (r *Replay) doKeepAlives(done <-chan struct{}) {
 		case <-done:
 			return
 		case <-ticker.C:
-			err := r.sendKeepAlive()
+			err := c.sendKeepAlive()
 			if err != nil {
 				log.Println("sendKeepAlive: ", err)
 				return
 			}
-			_, message, err := r.ReadMessage()
+			_, message, err := c.ReadMessage()
 			if err != nil {
 				log.Println("ReadMessage: ", err)
 				return
 			}
 			if !bytes.Equal(message, []byte(keepAliveMsg)) {
 				log.Println("Did not receive expected keep alive response, closing connection.")
-				r.failedKeepAlive <- struct{}{}
+				c.failedKeepAlive <- struct{}{}
 			}
 		}
 	}
 }
 
-func (r *Replay) sendKeepAlive() error {
+// sendKeepAlive sends a keep alive message to its peer.
+func (c *conn) sendKeepAlive() error {
 	now := time.Now()
 	ts := now.Format("15:04:05")
 	message := fmt.Sprintf("![k[alive],v[%s]!", ts)
-	err := r.WriteMessage(websocket.TextMessage, []byte(message))
+	err := c.WriteMessage(websocket.TextMessage, []byte(message))
 	if err != nil {
 		return err
 	}
